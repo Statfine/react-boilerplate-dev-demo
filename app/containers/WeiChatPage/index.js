7c64039bac08c7e31c48ed7d0fae4925258a3463
shaojia <jia.shao@eastiming.com>
2018-02-06 15:34:00 +0800

    fix

--- app/containers/WeiChatPage/index.js
+++ app/containers/WeiChatPage/index.js
@@ -8,6 +8,11 @@ function utilGetQueryString(name) {
   return null;
 }
 
+/*
+* https://mp.weixin.qq.com/wiki?t=resource/res_main&id=mp1421140842
+* http://qydev.weixin.qq.com/wiki/index.php?title=微信JS-SDK接口
+*/
+
 export default class WeiChatPage extends PureComponent {
   state = {};
   componentWillMount() {
