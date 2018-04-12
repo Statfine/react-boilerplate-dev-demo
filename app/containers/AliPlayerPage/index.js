c86cdff4d86dd78535da4f8357e9f2c624db1648
shaojia <jia.shao@eastiming.com>
2018-04-12 16:58:29 +0800

    fix aliplayer

--- app/containers/AliPlayerPage/index.js
+++ app/containers/AliPlayerPage/index.js
@@ -7,7 +7,7 @@ class AliPlayerPage extends PureComponent {
   render() {
     return (
       <div>
-        <Player playId="palyOne" url="http://alcdn.hls.xiaoka.tv/20171226/298/b36/KhW9oREcf_BTjYpZ/index.m3u8" />
+        <Player playId="palyOne" url="http://vod.gslb.cmvideo.cn/depository/asset/zhengshi/3011/173/928/3011173928/media/3011173928_5001598454_55.mp4.m3u8?msisdn=300001_300001&mdspid=&timestamp=20180412163744&Channel_ID=10290001088&ParentNodeID=-99&client_ip=10.151.129.83&ProgramID=800278835&assertID=3011173928&chargePhone=&SecurityKey=20180412163744&encrypt=cce50bdd445dec7570bc7994924162a0" />
         <Player playId="palyTwo" url="http://alcdn.hls.xiaoka.tv/20171226/298/b36/KhW9oREcf_BTjYpZ/index.m3u8" />
       </div>
     );
