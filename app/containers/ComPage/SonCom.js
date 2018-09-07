f6706707588dda928e909cefdac905911942b1f1
shaojia <jia.shao@eastiming.com>
2018-09-07 19:22:16 +0800

    add life  tip

--- app/containers/ComPage/SonCom.js
+++ app/containers/ComPage/SonCom.js
@@ -2,6 +2,11 @@ import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 
 export default class Sonpage extends PureComponent {
+
+  componentDidUpdate() {
+    console.log('renderSom componentDidUpdate');
+  }
+
   render() {
     const { index } = this.props;
     console.log('renderSom');
--- app/containers/ComPage/index.js
+++ app/containers/ComPage/index.js
@@ -26,9 +26,10 @@ export default class FatherPage extends PureComponent {
       >
         <Helmet title="子组件" />
         <p>组件内的state变动会导致组件重新渲染</p>
-        <p>子组建如果有匿名函数 父组件渲染的时候 子组件都会重新渲染</p>
+        <p>子组建如果有匿名函数 父组件渲染的时候 子组件都会重新渲染, 匿名函数导致新对象，所以也会重新渲染</p>
         <p>{index}</p>
         <SonPAge index={index} />
+        {/* <SonPAge index={index} show={() => console.log('aa')} /> */}
       </div>
     );
   }
